98e19125fabc357ba110a75ff7159ec75cc88667
Dexter201 <163344747+Dexter201@users.noreply.github.com>
2024-10-15 17:48:57 +0200

    minimax with first pruning

--- Project1/src/minimax.py
+++ Project1/src/minimax.py
@@ -13,20 +13,20 @@ class PacmanAgent(Agent):
         """Given a Pacman game state, returns a legal move.
 
         Arguments:
-            state: a game state. See API or class `pacman.GameState`.
+            state: a game state. See API or class pacman.GameState.
 
         Returns:
-            A legal move as defined in `game.Directions`.
+            A legal move as defined in game.Directions.
         """
-        if self.move is None:
-            self.move = minimaxSearch(state)[1]
-        
+
+        self.move = minimaxSearch(state)[1]
+
         if self.move is not None:
             return self.move
         else:
             return Directions.STOP
 
-def minimaxSearch(state):
+def minimaxSearch(s):
     """Given a Pacman game state, returns a list [Score, Move] if pacman use the path indicated by minimax's algo.
 
     Arguments:
@@ -37,41 +37,52 @@ def minimaxSearch(state):
     """
 
     #we know that it's Pacman which plays first
-    return maxSearch(state)
+    return maxSearch(s,alpha=np.NINF, beta=np.PINF)
 
-def maxSearch(state):
-    PacmanSuccessors = state.generatePacmanSuccessors()
+def maxSearch(s, alpha=0, beta=0):
+    PacmanSuccessors = s.generatePacmanSuccessors()
     NextScores       = np.zeros(shape=len(PacmanSuccessors))
     NextActions      = []
+    Score = np.NINF
     index = 0
     for Successor in PacmanSuccessors:
         NextState  = Successor[0]
         NextAction = Successor[1]
-        if(NextState.isWin() or NextState.isLose()):                # = terminal_test(s)
-            NextScores[index]  = NextState.getScore()               # = utility(s)
-            NextActions.append(NextAction)
+        if(NextState.isWin() or NextState.isLose()):                            # = terminal_test(s)
+            NextScores[index]  = NextState.getScore()                           # = utility(s)
         else:
-            NextScores[index]  = np.max(minSearch(NextState)[0])    # = max(MINIMAX(result(s, a) = NextState))
-            NextActions.append(NextAction)
+            NextScores[index]  = np.max(minSearch(NextState, alpha, beta)[0])    # = max(MINIMAX(result(s, a) = NextState))
+        Score = NextScores[index]
+        NextActions.extend([NextAction])
+        if Score >= beta:
+            indexMaxScore = np.where(NextScores==np.max(NextScores))[0][0]
+            return NextScores[indexMaxScore], NextActions[indexMaxScore]
+        else:
+            alpha = max(Score, alpha)
         index += 1
     
     indexMaxScore = np.where(NextScores==np.max(NextScores))[0][0]
     return NextScores[indexMaxScore], NextActions[indexMaxScore]
             
-def minSearch(state):
-    GhostSuccessors = state.generateGhostSuccessors(1)
+def minSearch(s, alpha=0, beta=0):
+    GhostSuccessors = s.generateGhostSuccessors(1)
     NextScores       = np.zeros(shape=len(GhostSuccessors))
     NextActions      = []
     index = 0
     for Successor in GhostSuccessors:
         NextState  = Successor[0]
         NextAction = Successor[1]
-        if(NextState.isWin() or NextState.isLose()):                # = terminal_test(s)
-            NextScores[index] = NextState.getScore()                # = utility(s)
-            NextActions.append(NextAction)
+        if(NextState.isWin() or NextState.isLose()):                            # = terminal_test(s)
+            NextScores[index] = NextState.getScore()                            # = utility(s)
         else:
-            NextScores[index] = np.min(maxSearch(NextState)[0])     # = min(MINIMAX(result(s, a) = NextState))
-            NextActions.append(NextAction)
+            NextScores[index] = np.min(maxSearch(NextState, alpha, beta)[0])     # = min(MINIMAX(result(s, a) = NextState))
+        Score = NextScores[index]
+        NextActions.extend([NextAction])
+        if Score <= alpha:
+            indexMaxScore = np.where(NextScores==np.max(NextScores))[0][0]
+            return NextScores[indexMaxScore], NextActions[indexMaxScore]
+        else:
+            beta = min(Score, beta)
         index += 1
 
     indexMinScore = np.where(NextScores==np.min(NextScores))[0][0]
